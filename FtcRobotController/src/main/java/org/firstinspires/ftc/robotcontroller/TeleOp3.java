aa99c84b7da7a2f606c227c72b4d78dd97898796
Ella Julie Range <thecabbage999@gmail.com>
2024-11-04 20:48:32 -0600

    movement code

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/TeleOp3.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/TeleOp3.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.robotcontroller;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @TeleOp
 public class TeleOp3 extends LinearOpMode {
@@ -11,11 +10,19 @@ public class TeleOp3 extends LinearOpMode {
     @Override
     public void runOpMode() {
         // motor def
-        DcMotor LeftMotor = hardwareMap.get(DcMotor.class, "LeftMotor");
-        DcMotor RightMotor = hardwareMap.get(DcMotor.class, "RightMotor");
+        DcMotor Left_Motor = hardwareMap.get(DcMotor.class, "Left_Motor");
+        DcMotor Right_Motor = hardwareMap.get(DcMotor.class, "Right_Motor");
+        DcMotor Arm_Line = hardwareMap.get(DcMotor.class, "Arm_Line");
+        DcMotor Arm_upL = hardwareMap.get(DcMotor.class,"Arm_upL");
+        DcMotor Arm_upR = hardwareMap.get(DcMotor.class,"Arm_upR") ;
+;
         // reverse test
         //LMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         //dRMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //set postion
+        int pos1 = 100;
+        int pos2 = 0;
 
         waitForStart();
         while (opModeIsActive()) {
@@ -23,8 +30,33 @@ public class TeleOp3 extends LinearOpMode {
             double leftY = gamepad1.left_stick_y;
             double RightX = gamepad1.right_stick_x;
             // motor to joystick according to values
-            LeftMotor.setPower(leftY + RightX);
-            RightMotor.setPower(leftY - RightX);
+            Left_Motor.setPower(leftY + RightX);
+            Right_Motor.setPower(leftY - RightX);
+            //set postion
+            if (gamepad1.dpad_up){
+
+            }
+
+
+            // arm code
+            // A button
+            if (gamepad1.a) {
+                Arm_Line.setTargetPosition(pos1);
+                Arm_Line.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                Arm_Line.setPower(1);
+            }else {
+                Arm_Line.setPower(0);
+
+            }
+           // y button
+            if ( gamepad1.y){
+                Arm_Line.setTargetPosition(pos2);
+                Arm_Line.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                Arm_Line.setPower(-1);
+            }else{
+                Arm_Line.setPower(0);
+            }
+
         }
     }
 }
