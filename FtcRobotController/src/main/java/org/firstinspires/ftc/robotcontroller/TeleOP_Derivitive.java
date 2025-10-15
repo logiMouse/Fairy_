0b44d92cb8f4da92983e0a07d33112b1c78453fa
Ella Julie Range <thecabbage999@gmail.com>
2025-10-14 20:01:02 -0500

    test_intake

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/TeleOP_Derivitive.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/TeleOP_Derivitive.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcontroller.subsy.The_better_Intake;
 import org.firstinspires.ftc.robotcontroller.subsy.The_better_train;
 
 @TeleOp
@@ -14,6 +15,7 @@ public class TeleOP_Derivitive extends LinearOpMode {
     @Override
     public void runOpMode(){
         The_better_train The_better_train =  new The_better_train(hardwareMap);
+        The_better_Intake The_better_intake = new The_better_Intake(hardwareMap);
 
         // joysticks (driver)
         double left_joystick_y;
@@ -32,6 +34,15 @@ public class TeleOP_Derivitive extends LinearOpMode {
             right_joystick_x = gamepad1.left_stick_x;
 
             The_better_train.strife_calc(left_joystick_y ,left_joystick_x, right_joystick_x );
+
+            if (button_a){
+                The_better_intake.forward();
+
+            }
+            if (button_x){
+                The_better_intake.backward();
+
+            }
 
 
 
--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/subsy/The_better_Intake.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/subsy/The_better_Intake.java
@@ -1,4 +1,38 @@
 package org.firstinspires.ftc.robotcontroller.subsy;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class The_better_Intake {
+
+    DcMotorEx right_intake;
+    DcMotorEx left_intake;
+
+
+    public The_better_Intake(HardwareMap hardwareMap){
+
+        right_intake = hardwareMap.get(DcMotorEx.class, "RI");
+        left_intake = hardwareMap.get(DcMotorEx.class, "LI");
+
+
+
+    }
+
+
+    public void forward (){
+        right_intake.setDirection(DcMotorSimple.Direction.REVERSE);
+        left_intake.setDirection(DcMotorSimple.Direction.REVERSE);
+        right_intake.setPower(0.5);
+        left_intake.setPower(0.5);
+
+    }
+
+    public void backward (){
+        right_intake.setPower(0.5);
+        left_intake.setPower(0.5);
+
+    }
+
+
 }
