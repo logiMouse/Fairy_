8611b176869cefef13a76e222f5f4602bd378760
Ella Julie Range <thecabbage999@gmail.com>
2025-10-22 16:34:05 -0500

    intake

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/subsy/The_better_Intake.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/subsy/The_better_Intake.java
@@ -7,13 +7,13 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class The_better_Intake {
 
     DcMotorEx right_intake;
-    DcMotorEx left_intake;
+
 
 
     public The_better_Intake(HardwareMap hardwareMap){
 
         right_intake = hardwareMap.get(DcMotorEx.class, "RI");
-        left_intake = hardwareMap.get(DcMotorEx.class, "LI");
+
 
 
 
@@ -22,15 +22,14 @@ public class The_better_Intake {
 
     public void forward (){
         right_intake.setDirection(DcMotorSimple.Direction.REVERSE);
-        left_intake.setDirection(DcMotorSimple.Direction.REVERSE);
-        right_intake.setPower(0.5);
-        left_intake.setPower(0.5);
+        right_intake.setPower(1);
+
 
     }
 
     public void backward (){
-        right_intake.setPower(0.5);
-        left_intake.setPower(0.5);
+        right_intake.setPower(1);
+
 
     }
 
