02a1dc73ce832c1f1144676a5e10e2aa0a6137b3
logiMouse <156532330+logiMouse@users.noreply.github.com>
2025-11-20 08:25:37 -0600

    Modify constructor to accept HardwareMap parameter

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/subsy/The_Better_Constance.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/subsy/The_Better_Constance.java
@@ -3,8 +3,12 @@ package org.firstinspires.ftc.robotcontroller.subsy;
 
 public class The_Better_Constance {
 
+    DcMotor shoot;
 
-    public The_Better_constance(){
+
+    public The_Better_constance(HardwareMap hardwareMap){
+
+        shoot = 
 
 
         
