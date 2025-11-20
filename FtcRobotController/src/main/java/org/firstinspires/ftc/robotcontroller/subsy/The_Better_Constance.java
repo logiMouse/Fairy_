1f565d9ab55c79ba2b3af4cbec6890f84e210b27
logiMouse <156532330+logiMouse@users.noreply.github.com>
2025-11-20 08:19:26 -0600

    shoot_github_abandon
    dsafsdfadsfdasfdsafsdfadsfa

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/subsy/The_Better_Constance.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/subsy/The_Better_Constance.java
@@ -1,39 +1,13 @@
-package org.firstinspires.ftc.robotcontroller.subsy;
-
 public class The_Better_Constance {
 
-    public void PID(){
 
+    public The_Better_constance(){
 
-    }
-
-    public void auto_drive(){
-        // straight
-        double stright_back_left = 6; 
-        double stright_back_right = 7;
-        double stright_fount_left = 6;
-        double stright_frount_right = 7;
-
-        // left turn 
-        double leftTurn_back_left = 6; 
-        double leftTurn_back_right = 7;
-        double leftTurn_fount_left = 6;
-        double leftTurn_frount_right = 7;
-
-        // right_Turn
-        double rightTurn_back_left = 6; 
-        double rightTurn_back_right = 7;
-        double rightTurn_fount_left = 6;
-        double rightTurn_frount_right = 7;
-
-        // backwards
-        double backwards_back_left = 6; 
-        double backwards_back_right = 7;
-        double backwards_fount_left = 6;
-        double backwards_frount_right = 7;
 
         
     }
+
+
 
 
 
