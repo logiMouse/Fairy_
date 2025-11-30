2581fde6dd9e0cf32cff337736f07016792082de
logiMouse <156532330+logiMouse@users.noreply.github.com>
2025-11-30 09:24:04 -0600

    Update The_wose_auto.java

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/subsy/The_wose_auto.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/subsy/The_wose_auto.java
@@ -1,4 +1,21 @@
 package org.firstinspires.ftc.robotcontroller.subsy;
+import org.firstinspires.ftc.robotcontroller.subsy.The_better_train;
+
+
 
 public class The_wose_auto {
+
+      The_better_train The_better_train;
+
+  public The_wose_auto(){
+
+    The_better_train The_better_train =  new The_better_train(hardwareMap);
+
+    
+  }
+
+  public void trial_1(){
+     The_better_train.strife_calc(0.0,x.x, 1.0 );
+  }
+  
 }
